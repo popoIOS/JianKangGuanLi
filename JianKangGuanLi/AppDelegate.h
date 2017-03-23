c88918273b2ee94c2a6f0fac3e6ea6a21f47ea9b
贾应许 <iphone_project@163.com>
2017-03-23 10:11:46 +0800

    Initial Commit

---
+++ JianKangGuanLi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JianKangGuanLi
+//
+//  Created by ydz on 17/3/23.
+//  Copyright © 2017年 yzd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JianKangGuanLi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JianKangGuanLi
+//
+//  Created by ydz on 17/3/23.
+//  Copyright © 2017年 yzd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
